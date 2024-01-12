c5b99abb2c1aba8a05ad259e73fb6a80b766ee94
Tahiatul Islam <tahiatul@tahiatuls-mbp.myfiosgateway.com>
2024-01-11 21:01:02 -0500

    adding a test class

---
+++ TestMain.py
@@ -0,0 +1,11 @@
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+
+    def test_build_specimin_command():
+        pass
+
+
+    if __name__ == '__main__':
+        unittest.main()
