39100057026735fa5649a0e68311c5f27966ba0b
Tahiatul Islam <tahiatul@gmail.com>
2024-03-11 12:56:21 -0400

    Compile specimin generated output in evaluation script (#20)
    ---------
    
    Co-authored-by: Tahiatul Islam <tahiatul@tahiatuls-mbp.myfiosgateway.com>

---
+++ check_compilation.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# This script runs javac on all of the minimized outputs under ISSUES/ .
+# It returns 2 if any of them fail to compile, 1 if there are any malformed directories,
+# and 0 if all of them do compile.
+#
+# It is desirable that all of the expected Specimin's minimized program gets compiled, because Specimin
+# should produce independently-compilable programs.
+
+# when this script is invoked from specimin CI, 
+# compilation result will be compared with "expected" result and CI will be passed/failed accordingly.
+
+returnval=0
+compile_status_json="{"
+echo "Specimin path: $SPECIMIN"
+
+#read all issue_idjson from json file
+issue_ids="$(jq -r '.[].issue_id' resources/test_data.json)"
+cd ISSUES || exit 1
+
+for target in $issue_ids; do
+    echo "Target = ${target}"
+
+    cd "${target}/output/" || exit 1
+    # check if any directory exists inside output. If no directory there, specimin failed on the input target
+    # in that case, ignoring it.
+    directory_count="$(find ./ -mindepth 1 -type d | wc -l)"
+    if [ "$directory_count" -eq 0 ]; then 
+        echo "No directories inside ${target}/output. Ignoring it."
+        compile_status_json="$compile_status_json\n  \"$target\": \"FAIL\","
+        cd ../..
+        continue
+    fi
+
+    # javac relies on word splitting
+    # shellcheck disable=SC2046
+    javac -classpath "$SPECIMIN/main/src/test/resources/shared/checker-qual-3.42.0.jar" $(find . -name "*.java")
+    javac_status=$?
+    if [ $javac_status -eq 0 ]; then 
+       echo "Running javac on ${target}/output PASSES"
+       compile_status_json="$compile_status_json\n  \"$target\": \"PASS\","
+    else
+        echo "Running javac on ${target}/output FAILS. Please check logs above."
+        compile_status_json="$compile_status_json\n  \"$target\": \"FAIL\","
+        returnval=2
+    fi
+    
+    cd ../.. || exit 1
+
+done
+
+if [ "${returnval}" = 0 ]; then
+  echo "All expected test outputs compiled successfully."
+elif [ "${returnval}" = 2 ]; then
+  echo "Some expected test outputs do not compile successfully. See the above error output for details."
+fi
+
+compile_status_json="${compile_status_json%,}"
+compile_status_json="$compile_status_json\n}"
+rm compile_status.json
+echo "$compile_status_json" > compile_status.json
+find . -name "*.class" -exec rm {} \;
